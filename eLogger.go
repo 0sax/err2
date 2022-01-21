f6288301becc0a41d65544807d5f509cb883f536
Lord-ofada <omfaluyi@gmail.com>
2022-01-21 09:12:05 +0100

    fixed issue where error logger would cause panic if a nil error is provided

--- eLogger.go
+++ eLogger.go
@@ -3,6 +3,10 @@ package err2
 import "log"
 
 func LogErr1(parent, child string, err error) {
+	errMsg := "no error provided"
+	if err != nil {
+		errMsg = err.Error()
+	}
 	log.Printf("%v failed at '%v', because: %v",
-		parent, child, err.Error())
+		parent, child, errMsg)
 }
